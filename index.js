79f5a958bfc3fcb0a24123d70a47a0e760ec7d0f
Solamento02 <noahdev83@outlook.com>
2025-10-18 21:33:29 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Olá mundo!');
+const express = require('express');
+const app = express()
+const port = 3000;
+
+app.get('/', (req,res) => {
+	res.send('Olá mundo!')
+});
+
+app.listen(port, () => {
+	console.log(`Servidor rodando http://localhost:${port}`)
+})
